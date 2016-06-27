ee11ace4cfac6ab64699e6998487add8b8b5dfe6
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2016-06-27 15:43:54 -0700

    Initial submission for the delete scripts

---
+++ install/DeleteAssetDatabase.ps1
@@ -0,0 +1,7 @@
+﻿$afserver = Get-AFServer -Name localhost
+$conn = Connect-AFServer -AFServer $afserver
+$afdatabases = Get-AFDatabase -AFServer $conn -Name Asset*
+foreach ($afdatabase in $afdatabases) {
+    Write-Host "Deleting" $afdatabase
+    $status = Remove-AFDatabase -AFDatabase $afdatabase
+}
---
+++ install/DeleteOSIDemoTags.ps1
@@ -0,0 +1,7 @@
+﻿$conn = Connect-PIDataArchive -PIDataArchiveMachineName localhost
+$tags = Get-PIPoint -Name OSIDEMO_* -Connection $conn
+Write-Host $tags
+foreach ($tag in $tags) {
+    Write-Host "Deleting " $tag.Point.Name
+    Remove-PIPoint -Connection $conn -Name $tag.Point.Name
+}
