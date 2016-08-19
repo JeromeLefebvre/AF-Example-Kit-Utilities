f083fe93a123abf40d760f978ba014fb813edfd6
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2016-08-19 16:47:12 +0900

    Added in the code to export the utilities kit

---
+++ ExportKit/ExportKit/Program.cs
@@ -0,0 +1,20 @@
+﻿using OSIsoft.AF;
+
+namespace ExportKitScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var version = "v2016B";
+            var pisystem = new PISystems().DefaultPISystem;
+            foreach (var UOMClassName in new string[] { "Power", "Energy Cost", "Volume Cost", "US Currency", "Energy per Volume" }) {
+                var UOMClass = pisystem.UOMDatabase.UOMClasses[UOMClassName];
+                pisystem.ExportXml(UOMClass, PIExportMode.NoUniqueID, $"..\\UOM_{UOMClass.Name}_{version}.xml", null, null, null);
+            }
+
+            var db = pisystem.Databases.DefaultDatabase;
+            pisystem.ExportXml(db, PIExportMode.NoUniqueID, $"..\\OSIDemo_Utilities Management System_{version}.xml", null, null, null);
+        }
+    }
+}
