799be10cafe4915d2005ec2b34b2b1ea0aed701f
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2016-08-23 11:14:29 +0900

    Fixes #14
    manually recalculates at fixed interval and ignores the analyses
    setting, but this is fine for our usage.

--- testKit/testKit/Program.cs
+++ testKit/testKit/Program.cs
@@ -7,6 +7,9 @@ using OSIsoft.AF;
 using OSIsoft.AF.Asset;
 using System.ServiceProcess;
 using OSIsoft.AF.PI;
+using OSIsoft.AF.Time;
+using OSIsoft.AF.Analysis;
+using OSIsoft.AF.Data;
 
 namespace testKit
 {
@@ -33,7 +36,8 @@ namespace testKit
             
             StartService("PIAnalysisManager", 10000);
 
-            // Todo: Backfill. Kenji.
+            foreach (var category in new List<string> { "Random Data", "Usage", "Cost", "Downtime" })
+                ProgrammaticAnalysisRecalculation(system, db, db.AnalysisCategories["Random Data"]);
         }
 
         public static void createConfig(AFElement elem)
@@ -76,6 +80,89 @@ namespace testKit
             {
                 Console.WriteLine("Could not start the analysis service");
             }
+        }
+
+        public static void ProgrammaticAnalysisRecalculation(PISystem system, AFDatabase database, AFCategory category)
+        {
+            // we start by generating timestamps we want to recalculate  
+            // we could get them from existing recorded values, etc...  
+            //  
+            // here we simply generate yesterdays hourly values ...  
+            var recalculationTimeStamps = new List<AFTime>();
+            for (int i = 0; i < 24; i++)
+            {
+                recalculationTimeStamps.Add(new AFTime(DateTime.Today.Subtract(TimeSpan.FromDays(3)) + TimeSpan.FromHours(i)));
+            }
+
+
+            AFNamedCollectionList<AFAnalysis> analysislist;
+            analysislist = AFAnalysis.FindAnalyses(database, null, null, null, category, null, null, AFStatus.None, AFSortField.ID, AFSortOrder.Ascending, 0, 1000);
+
+
+            foreach (var afAnalysis in analysislist)
+            {
+                // we recalculate results  
+                var results = Calculate(afAnalysis, recalculationTimeStamps);
+
+                // we could delete values here, but I simply replce them instead  
+
+                // we insert our new values  
+                AFListData.UpdateValues(results, AFUpdateOption.Replace);
+            }
+
+        }
+
+
+        /// <summary>  
+        /// Adapted from Mike's example  
+        /// Mike also talks about errors and warnings in his post, you should check it.  
+        /// </summary>  
+        /// <see cref="https://pisquare.osisoft.com/message/28537#28537"/>  
+        /// <param name="analysis"></param>  
+        /// <param name="times"></param>  
+        /// <returns></returns>  
+        private static List<AFValue> Calculate(AFAnalysis analysis, IEnumerable<AFTime> times)
+        {
+
+            var results = new List<AFValue>();
+            var analysisConfiguration = analysis.AnalysisRule.GetConfiguration();
+            var state = new AFAnalysisRuleState(analysisConfiguration);
+            foreach (var time in times)
+            {
+                Console.WriteLine("Evaluating for {0}", time);
+                state.Reset();
+                state.SetExecutionTimeAndPopulateInputs(time);
+                analysis.AnalysisRule.Run(state);
+                if (state.EvaluationError == null)
+                {
+                    // this merges the state (results) with the configuration so its easier to loop with both...  
+                    var resultSet = analysisConfiguration.ResolvedOutputs.Zip(state.Outputs, Tuple.Create);
+
+
+                    foreach (var result in resultSet)
+                    {
+                        // for more clarty, we take out our data into clearer variables  
+                        AFAnalysisRuleResolvedOutput analysisRow = result.Item1;
+                        var calcRes = (AFValue)result.Item2;
+
+
+                        // we filter to get only the results that have an output attribute ( an AFValue )  
+                        if (analysisRow.Attribute != null)
+                        {
+                            // add new AF Value into the results table  
+                            results.Add(new AFValue((AFAttribute)analysisRow.Attribute, calcRes.Value, calcRes.Timestamp));
+                        }
+                    }
+                }
+                else
+                {
+                    // errors occur quite frequently, for example, TagTot('attr1', 't', '*') will fail if computed at '*' = 't'
+                    // but this does not occur when the analysis is running on event base.
+                    //Console.WriteLine("An error occurred: {0}", state.EvaluationError.Message);
+                }
+            }
+
+            return results;
         }
     }
 }
