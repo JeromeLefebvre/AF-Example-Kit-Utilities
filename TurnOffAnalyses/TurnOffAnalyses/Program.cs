7e9d23bea8511815e54cd45b89e342c4b2b7ae0b
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2016-10-22 16:31:15 +0900

    Drop usage of AFDatabase.Analysis as it is outdated
    Also rename the project to use the pural form

---
+++ TurnOffAnalyses/TurnOffAnalyses/Program.cs
@@ -0,0 +1,16 @@
+﻿using OSIsoft.AF;
+using OSIsoft.AF.Analysis;
+
+namespace TurnOffAnalyses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new PISystems().DefaultPISystem.Databases.DefaultDatabase;
+            var analyses = AFAnalysis.FindAnalyses(db, "*", AFSearchField.Name, AFSortField.Name, AFSortOrder.Ascending, int.MaxValue);
+            foreach (var analysis in analyses)
+                analysis.SetStatus(AFStatus.Disabled);
+        }
+    }
+}
